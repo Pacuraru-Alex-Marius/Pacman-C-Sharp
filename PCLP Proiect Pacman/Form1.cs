df1b651f238ec31bc795d6f1bef01130ebacee58
Pacuraru-Alex-Marius <alex.pacuraru24@gmail.com>
2023-05-25 14:30:35 +0300

    Pacman C Sharp

---
+++ PCLP Proiect Pacman/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PCLP_Proiect_Pacman
+{
+    public partial class Form1 : Form
+    {
+
+        bool goup, godown, goleft, goright, isGameOver;
+
+        int score, playerSpeed, redGhostSpeed, yellowGhostSpeed, pinkGhostX, pinkGhostY, blueGhostX, blueGhostY;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            resetGame();
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+
+            if(e.KeyCode == Keys.Up)
+            {
+                goup = true;
+            }
+            if(e.KeyCode == Keys.Down)
+            {
+                godown = true;
+            }
+            if(e.KeyCode==Keys.Left)
+            {
+                goleft = true; 
+            }
+            if(e.KeyCode== Keys.Right)
+            {
+                goright = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goup = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                godown = false;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = false;
+            }
+            if(e.KeyCode == Keys.Enter && isGameOver == true)
+            {
+                resetGame();
+            }
+
+        }
+
+        private void mainGameTimer(object sender, EventArgs e)
+        {
+            txtScore.Text = "Score: " + score;
+
+            if (goleft == true)
+            {
+                pacman.Left -= playerSpeed;
+                pacman.Image = Properties.Resources.left;
+            }
+            if (goright == true)
+            {
+                pacman.Left += playerSpeed;
+                pacman.Image = Properties.Resources.right;
+            }
+            if (godown == true)
+            {
+                pacman.Top += playerSpeed;
+                pacman.Image = Properties.Resources.down;
+            }
+            if (goup == true)
+            {
+                pacman.Top -= playerSpeed;
+                pacman.Image = Properties.Resources.up;
+            }
+
+            if (pacman.Left < -10)
+            {
+                pacman.Left = 680;
+            }
+            if (pacman.Left > 680)
+            {
+                pacman.Left = -10;
+            }
+
+            if (pacman.Top < -10)
+            {
+                pacman.Top = 550;
+            }
+            if (pacman.Top > 550)
+            {
+                pacman.Top = 0;
+            }
+
+            foreach (Control x in this.Controls) 
+            {
+                if (x is PictureBox)
+                {
+
+
+                    if ((string)x.Tag == "coin" && x.Visible == true)
+                    {
+                        if (pacman.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            score += 1;
+                            x.Visible = false;
+                        }
+                    }
+
+                    if ((string)x.Tag == "wall")
+                    {
+                        if (pacman.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            gameOver("You Lose, try again!");
+                        }
+
+
+                        if (pinkGhost.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            pinkGhostX = -pinkGhostX;
+                            pinkGhostY = -pinkGhostY;
+                        }
+
+                        if (blueGhost.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            blueGhostX = -blueGhostX;
+                            blueGhostY = -blueGhostY;
+
+                        }
+                    }
+
+                    if ((string)x.Tag == "ghost")
+                    {
+                        if (pacman.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            gameOver("You Lose, try again!");
+                        }
+                    }
+
+                }
+            }
+
+
+            // moving ghosts
+
+            redGhost.Left += redGhostSpeed;
+
+            if (redGhost.Bounds.IntersectsWith(pictureBox1.Bounds) || redGhost.Bounds.IntersectsWith(pictureBox2.Bounds))
+            {
+                redGhostSpeed = -redGhostSpeed;
+            }
+
+            yellowGhost.Left -= yellowGhostSpeed;
+
+            if (yellowGhost.Bounds.IntersectsWith(pictureBox3.Bounds) || yellowGhost.Bounds.IntersectsWith(pictureBox4.Bounds))
+            {
+                yellowGhostSpeed = -yellowGhostSpeed;
+            }
+
+            pinkGhost.Left -= pinkGhostX;
+            pinkGhost.Top -= pinkGhostY;
+
+            if(pinkGhost.Top < 0 || pinkGhost.Top > 520)
+            {
+                pinkGhostY = -pinkGhostY;
+            }
+
+            if(pinkGhost.Left < 0 || pinkGhost.Left > 620)
+            {
+                pinkGhostX = -pinkGhostX; 
+            }
+
+            blueGhost.Left -= blueGhostX;
+            blueGhost.Top -= blueGhostY;
+
+            if (blueGhost.Top < 0 || blueGhost.Top > 520)
+            {
+                blueGhostY = -blueGhostY;
+            }
+
+            if (blueGhost.Left < 0 || blueGhost.Left > 620)
+            {
+                blueGhostX = -blueGhostX;
+            }
+
+
+
+            if (score == 46)
+            {
+                gameOver("You win, congratz!");
+            }
+
+        }
+
+        private void resetGame()
+        {
+
+            txtScore.Text = "Score: 0";
+            score = 0;
+
+            redGhostSpeed = 5;
+            yellowGhostSpeed = 5;
+            pinkGhostX = 5;
+            pinkGhostY= 5;
+            blueGhostX = 5;
+            blueGhostY= 5; 
+            playerSpeed = 7;
+
+            isGameOver = false;
+
+            pacman.Left = 31;
+            pacman.Top = 46;
+
+            redGhost.Left = 214;
+            redGhost.Top = 68;
+
+            yellowGhost.Left = 426;
+            yellowGhost.Top = 430;
+
+            blueGhost.Left = 34;
+            blueGhost.Top = 294;
+
+            pinkGhost.Left = 556;
+            pinkGhost.Top = 12;
+
+            gameTimer.Start();
+
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox)
+                {
+                    x.Visible = true;
+                }
+            }
+
+        }
+
+        private void gameOver(string message)
+        {
+
+            isGameOver = true;
+
+            gameTimer.Stop();
+
+            txtScore.Text = "Score: " + score + Environment.NewLine + message;
+
+
+
+        }
+    }
+}
